860b05cf3efaa81f2c6808cd7779068631fd94c1
lekhang-1510 <tieukhang2023@gmail.com>
2026-02-24 21:28:59 +0700

    update day5-baitap commit

--- day5-baitap.js
+++ day5-baitap.js
@@ -1,13 +1,26 @@
 //BUỔI LÀM BÀI TẬP (24/2)
 //BÀI TẬP 1:
-let time = 20;
-let i = time / 2;
-while (time > 0) {
-  if (time === i) {
-    console.log("Đã qua nữa thời gian");
-  } else {
-    console.log(`Còn lại ${time} giây`);
-  }
-  time--;
+// let time = 15;
+// const i = Math.floor(time / 2); // làm tròn dưới vd: 10.5 === 10
+// while (time > 0) {
+//   if (time === i) {
+//     console.log("Đã qua nữa thời gian");
+//   } else {
+//     console.log(`Còn lại ${time} giây`);
+//   }
+//   time--;
+// }
+// console.log("Đã hết giờ ");
+
+//BÀI TẬP 2:
+let price = Math.ceil(Math.random() * 5000000);
+if (price < 100) {
+  price = 100;
 }
-console.log("Đã hết giờ ");
+let numOfOder = 5;
+let total = 0;
+for (i = 1; i <= numOfOder; i++) {
+  price;
+  total += price;
+}
+console.log(total);
