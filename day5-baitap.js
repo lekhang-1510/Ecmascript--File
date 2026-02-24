245fc6a8a147ed4c29de6ff1b6373ed5b5757754
lekhang-1510 <tieukhang2023@gmail.com>
2026-02-24 20:44:43 +0700

    update day5-baitap commit

---
+++ day5-baitap.js
@@ -0,0 +1,12 @@
+//BUỔI LÀM BÀI TẬP (24/2)
+//BÀI TẬP 1:
+let time = 10;
+while (time > 0) {
+  if (time === 5) {
+    console.log("Đã qua nữa thời gian");
+  } else {
+    console.log(`Còn lại ${time} giây`);
+  }
+  time--;
+}
+console.log("Đã hết giờ ");
