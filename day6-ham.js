2b5eb75d18cc5c0ed84962c876fee474a1b14c1b
lekhang-1510 <tieukhang2023@gmail.com>
2026-02-26 20:53:41 +0700

    update commit

--- day6-ham.js
+++ day6-ham.js
@@ -29,5 +29,29 @@ function introduce(name, age, address) {
     `Xin chào, tôi tên là ${name}, năm nay tôi ${age}, Tôi đang sinh sống ở ${address} `,
   );
 }
-
 introduce("khang", 25, "TP Hồ Chí Minh");
+// có thể dùng return nếu không cần in : trả dữ liệu ra khỏi hàm + kết thúc hàm
+// NOTE 1: Tính tổng a,b
+function sum(a, b) {
+  console.log(`Tổng của ${a} và ${b} = ${a + b}`);
+}
+sum(6, 6);
+//Giá trị cố định
+function hello(name = "khang") {
+  // cố định biến name = khang
+  console.log(`Hi ${name}`);
+}
+hello();
+
+/**
+ * 1. Tạo 1 hàm tên là calculatePytago
+ * 2. Hàm này sẽ nhận vào tham số : canhGocVuong1, canhGocVuong2
+ * 3.Tính cạnh huyền và trả về kết quả
+ */
+function calculatePytago(canhGocVuong1, canhGocVuong2) {
+  let canhHuyen = Math.sqrt(
+    Math.pow(canhGocVuong1, 2) + Math.pow(canhGocVuong2, 2),
+  );
+  return canhHuyen;
+}
+console.log(`Cạnh huyền = ${calculatePytago(3, 4)}`);
