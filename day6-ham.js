e7fa64baee821b6e23fcff91d81ad886acc19cf4
lekhang-1510 <tieukhang2023@gmail.com>
2026-02-26 20:13:10 +0700

    fist commit

---
+++ day6-ham.js
@@ -0,0 +1,20 @@
+/**function myFunc(a) {
+  console.log(a);
+}
+//a tham trị - biến trung gian
+myFunc(5);//5,6,9,10 là tham số truyền vào
+myFunc(6);
+myFunc(9);
+myFunc(10);
+*/
+/**
+ * 1. Tạo 1 hàm tên là printName
+ * 2. có 1 tham số tên là name
+ * 3.Trong hàm, in ra lời chào name đó
+ */
+function printName(name) {
+  console.log(`Xin chào ${name}`);
+}
+printName("khang");
+printName("kim");
+printName("sơn");
