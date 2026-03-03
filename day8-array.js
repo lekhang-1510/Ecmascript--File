5d83271bbceaed300ab8229062d092b2a52b4f9a
lekhang-1510 <tieukhang2023@gmail.com>
2026-03-03 20:22:17 +0700

    Update commit

--- day8-array.js
+++ day8-array.js
@@ -1,11 +1,17 @@
-const allProvinces = [
-  "Bạc Liêu",
-  "Sóc Trăng",
-  "Cà Mau",
-  "Vĩnh Long",
-  "Cần Thơ",
-  "An Giang",
-];
-allProvinces.forEach((value, index) => {
-  console.log(`Tên tỉnh: ${value}`);
+// const allProvinces = [
+//   "Bạc Liêu",
+//   "Sóc Trăng",
+//   "Cà Mau",
+//   "Vĩnh Long",
+//   "Cần Thơ",
+//   "An Giang",
+// ];
+// allProvinces.forEach((allprovince, index) => {
+//   console.log(`Tên tỉnh ${index}: ${allprovince}`);
+// });
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumber = numbers.filter((number, index) => {
+  return number % 2 === 0;
 });
+console.log(` Số chẵn lọc từ mãng numbers: ${evenNumber}`);
