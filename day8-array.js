f19f7172861f2fb6f00cc90ab525cacca6ca5560
lekhang-1510 <tieukhang2023@gmail.com>
2026-03-03 20:07:33 +0700

    First commit

---
+++ day8-array.js
@@ -0,0 +1,11 @@
+const allProvinces = [
+  "Bạc Liêu",
+  "Sóc Trăng",
+  "Cà Mau",
+  "Vĩnh Long",
+  "Cần Thơ",
+  "An Giang",
+];
+allProvinces.forEach((value, index) => {
+  console.log(`Tên tỉnh: ${value}`);
+});
