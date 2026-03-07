0d274e24d8ab6c22668c5a1f2838ad097de727f5
lekhang-1510 <tieukhang2023@gmail.com>
2026-03-07 21:02:03 +0700

    Update battap5-6

--- day10.js
+++ day10.js
@@ -14,26 +14,34 @@
  *     - salary
  * 3. Tạo 1 biến đếm làm id (giá trin mặc định là 1)
  * 4. Thêm cái object vào list
+ * 5. Cho phép người dùng nhập vào số lượng nhân viên muốn thêm vào
+ * 6. Tái sử dụng lại đoạn mã trên để tạo mới và thêm những nhân viên đó vào
  */
 const prompt = require("prompt-sync")();
+let count = Number(prompt("Nhập số lượng nhân viên muốn thêm vào: "));
 const employees = [];
+let id = 1;
+for (let i = 0; i < count; i++) {
+  //const prompt = require("prompt-sync")();
+  console.log(`Nhập nhân viên thứ ${i + 1}`);
 
-let name = prompt("Nhập name: ");
-let email = prompt("Nhập email: ");
-let phoneNumber = prompt("Nhập phone: ");
-let gender = prompt("Nhập gender: ");
-let position = prompt("Nhập position: ");
-let salary = prompt("Nhập salary: ");
+  let name = prompt("Nhập name: ");
+  let email = prompt("Nhập email: ");
+  let phoneNumber = prompt("Nhập phone: ");
+  let gender = prompt("Nhập gender: ");
+  let position = prompt("Nhập position: ");
+  let salary = prompt("Nhập salary: ");
 
-const id = 1;
-const employees2 = {
-  id: id,
-  name: name,
-  email: email,
-  phoneNumber: phoneNumber,
-  gender: gender,
-  position: position,
-  salary: salary,
-};
-employees.push(employees2);
+  const employee = {
+    id,
+    name,
+    email,
+    phoneNumber,
+    gender,
+    position,
+    salary,
+  };
+  id++;
+  employees.push(employee);
+}
 console.log(employees);
