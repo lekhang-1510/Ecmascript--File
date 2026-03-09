65810f83c0b7a42a7d523d449fffae50106ddf3e
lekhang-1510 <tieukhang2023@gmail.com>
2026-03-09 20:09:05 +0700

    Update baitapcau 7,9

--- day10.js
+++ day10.js
@@ -18,30 +18,73 @@
  * 6. Tái sử dụng lại đoạn mã trên để tạo mới và thêm những nhân viên đó vào
  */
 const prompt = require("prompt-sync")();
-let count = Number(prompt("Nhập số lượng nhân viên muốn thêm vào: "));
+let numberOfEmployees = Number(
+  prompt("Nhập số lượng nhân viên muốn thêm vào: "),
+);
 const employees = [];
 let id = 1;
-for (let i = 0; i < count; i++) {
-  //const prompt = require("prompt-sync")();
-  console.log(`Nhập nhân viên thứ ${i + 1}`);
 
-  let name = prompt("Nhập name: ");
-  let email = prompt("Nhập email: ");
-  let phoneNumber = prompt("Nhập phone: ");
-  let gender = prompt("Nhập gender: ");
-  let position = prompt("Nhập position: ");
-  let salary = prompt("Nhập salary: ");
+for (let i = 0; i < numberOfEmployees; i++) {
+  console.log(`\nNhập nhân viên thứ ${i + 1}`);
 
+  // const name = prompt("Nhập name: ");
+  // const email = prompt("Nhập email: ");
+  // const phoneNumber = prompt("Nhập phone: ");
+  // const gender = prompt("Nhập gender: ");
+  // const position = prompt("Nhập position: ");
+  // const salary = Number(prompt("Nhập salary: "));
+
+  // tạo object employee
   const employee = {
     id,
-    name,
-    email,
-    phoneNumber,
-    gender,
-    position,
-    salary,
+    name: prompt("Nhập name: "),
+    email: prompt("Nhập email: "),
+    phoneNumber: prompt("Nhập phone: "),
+    gender: prompt("Nhập gender: "),
+    position: prompt("Nhập position: "),
+    salary: Number(prompt("Nhập salary: ")),
   };
+
+  //tăng id
   id++;
+
+  //push object employee vào mãng employees
   employees.push(employee);
 }
+
+//in ra mãng employees
+console.log("\nDanh sách nhân viên:");
 console.log(employees);
+
+// 7. Nhập để tìm kiếm nhân viên (Tìm theo tên)
+//Nhập tên nhân viên cần tìm
+const searchName = prompt("Nhập tên nhân viên cần tìm: ");
+
+//Tìm nhân viên trong mảng employees
+const foundEmployee = employees.find(
+  (employee) => employee.name === searchName,
+);
+
+//Nếu foundEmployee = True - Tìm thấy nhân viên
+if (foundEmployee) {
+  console.log("Nhân viên tìm được: ");
+  console.log(foundEmployee);
+} else {
+  console.log("Không tìm thấy nhân viên! ");
+}
+
+// 8. Chỉnh sửa thông tin nhân viên (Nhập vào id nhân viên để sửa thông tin  ) - KO LÀM
+
+// 9. Tìm kiếm tương đối ( includes ), trả về danh sách nhân viên tương ứng (Dùng filter)
+// nhập từ khóa tìm kiếm
+const keyWord = prompt(
+  "Nhập tên cần tìm (trả kết quả tên cần tìm & tên gần giống): ",
+);
+
+// lọc nhân viên - toLowerCase(ko phân biệt chữ thường và hoa) - includes(Tìm gần giống keyWord))
+const result = employees.filter((employee) =>
+  employee.name.toLowerCase().includes(keyWord.toLowerCase()),
+);
+
+console.log("Danh sách nhân viên tìm được:");
+console.log(result);
