72dcd1e26c00e6a4693d0622dd12683f827ffc94
lekhang-1510 <tieukhang2023@gmail.com>
2026-03-10 20:23:18 +0700

    Baitap yc1

---
+++ day11.js/index.js
@@ -0,0 +1,61 @@
+const employees = [
+  {
+    id: 1,
+    name: "Nguyễn Văn A",
+    email: "nva@company.com",
+    phoneNumber: "0901234567",
+    gender: "Male",
+    position: "Developer",
+    salary: 2000,
+  },
+  {
+    id: 2,
+    name: "Trần Thị B",
+    email: null,
+    phoneNumber: "0912345678",
+    gender: "Female",
+    position: "Tester",
+    salary: 1200,
+  },
+  {
+    id: 3,
+    name: "Lê Văn C",
+    email: "lvc@company.com",
+    phoneNumber: null,
+    gender: "Male",
+    position: "Developer",
+    salary: 1800,
+  },
+  {
+    id: 4,
+    name: "Phạm Thị D",
+    email: null,
+    phoneNumber: null,
+    gender: "Female",
+    position: "HR",
+    salary: 1000,
+  },
+  {
+    id: 5,
+    name: "Hoàng Văn E",
+    email: "hve@company.com",
+    phoneNumber: "0934567890",
+    gender: "Male",
+    position: "Manager",
+    salary: 3500,
+  },
+];
+
+//Lọc nhân viên (ko có sdt or email)
+// const employeesWithoutPhoneAndEmail = employees.filter(
+//   (employee) => employee.email !== null || employee.phoneNumber !== null,
+// );
+// console.log(employeesWithoutPhoneAndEmail);
+
+const employeesWithPhoneAndEmail = employees.filter((employee) => {
+  const { email, phoneNumber } = employee;
+
+  return email || phoneNumber;
+});
+
+console.log(employeesWithPhoneAndEmail);
